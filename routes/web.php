8d18592d291739ef556ce6633962e91896d3a8b9
mkesyasakha <adzikrasano@gmail.com>
2025-01-25 09:53:39 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,13 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth', 'role:admin')->group(function () {
-    Route::resource('users', CustomerController::class);
+    
     Route::resource('items', ItemController::class);
     Route::resource('suppliers', SupplierController::class);
     Route::resource('transactions', TransactionController::class);
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
-Route::middleware('auth', 'role:customer')->group(function () {
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::resource('users', CustomerController::class);
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('users', CustomerController::class);
+
